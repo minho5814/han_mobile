17193441217ae08298b8f30e892386788eba9dd1
minho5814 <tkmino84@gmail.com>
2017-04-17 10:06:17 +0900

    한살림 모바일 시작

---
+++ js/common.js
@@ -0,0 +1,38 @@
+$(window).load(function(){
+	/* ===============================================================================================
+		공통
+	=============================================================================================== */
+	/* 상단 고정 메뉴 슬라이드 */
+	$('.head-menu .swiper-container').each(function(){
+		var swiper = new Swiper(this, {
+			slidesPerView:'auto',
+			nextButton: '.head-menu .btn-next',
+			prevButton: '.head-menu .btn-prev'
+		});
+	});
+
+	/* 버튼 */
+	// 관심물품
+	$('.btn-like').click(function(){
+		$(this).addClass('on');
+	});
+
+	/* ===============================================================================================
+		스크롤 이벤트
+	=============================================================================================== */
+	/* 하단 고정메뉴 */
+	var scrolling = 0;
+	$(window).scroll(function(){
+		var winTop = $(window).scrollTop();
+		if (winTop > scrolling){
+			if(winTop == ($(document).height() - $(window).height())){
+				$('html').removeClass('down');
+			}else{
+				$('html').addClass('down');
+			}
+		}else {
+			$('html').removeClass('down');
+		}
+		scrolling = winTop;
+	});
+});
